ac81c1a874ccb4a571b40b3c31c640879c3a3d2a
JabbaCorinne <42677925+JabbaCorinne@users.noreply.github.com>
2019-01-22 22:47:31 -0800

    Main.java
    Code for vote counter

---
+++ Election-d/Main.java
@@ -0,0 +1,207 @@
+public class Main {
+
+    //insert election data as one string with semi-colon delimiter between each voter's data
+    public static String electionData= "236392,1,4,4,4;418945,3,4,4,4;408892,2,4,2,4;248043,1,1,3,2;138645,4,3,4,3;156160,4,4,2,3;121122,3,4,3,1;102572,2,4,1,4;278579,1,3,3,1;311936,2,2,1,2;180091,4,2,4,3;131991,3,4,1,3;349678,3,2,2,1;348822,4,2,1,1;358463,4,2,2,2;142865,1,1,1,2;397632,2,2,4,2;233101,2,3,2,1;336269,3,2,1,1;139745,3,1,1,1;388694,1,4,4,2;435193,1,1,2,1;298522,4,2,2,4;463523,3,2,1,3;340719,1,1,4,3;248911,4,2,4,2;176898,3,3,4,1;176697,2,1,3,4;479451,2,4,1,3;193036,1,3,2,1;130872,4,4,2,1;440023,4,4,3,4;376006,2,2,4,3;370599,1,3,3,4;114011,4,1,3,4;333671,2,1,3,3;116340,4,4,3,3;127895,3,1,1,4;475663,3,3,3,4;308242,1,2,4,4;176660,2,1,1,4;346022,3,2,4,2;313979,4,3,2,1;135913,1,2,4,2;180578,3,2,2,3;485579,4,4,2,3;313900,2,4,2,4;188421,4,2,4,3;475741,3,1,2,3;307455,3,4,3,2;356172,1,2,1,3;176303,1,2,3,3;141175,1,3,4,1;458524,1,3,1,1;359897,3,2,4,4;164080,2,2,2,4;310114,3,4,4,4;372346,3,1,2,2;341926,2,3,3,4;455848,2,1,4,2;298914,1,1,1,3;434548,3,3,3,3;454765,1,4,1,2;111469,3,3,4,2;215916,4,2,1,2;156160,4,3,1,4;409619,4,1,3,1;189440,2,1,4,2;354588,2,4,2,4;240064,2,2,1,1;317390,2,1,2,2;452034,1,1,1,3;305126,4,1,4,3;274294,3,4,4,4;473603,4,1,3,1;369608,4,2,4,3;456720,2,3,4,1;430577,1,3,4,4;420094,4,4,1,3;238780,4,1,2,4;284283,4,3,1,2;494623,1,3,2,2;345578,3,3,4,1;136356,3,3,1,2;391631,1,2,2,1;474920,4,1,2,2;484250,3,1,1,3;238515,2,4,1,3;332342,1,3,2,3;160398,4,4,3,2;251800,2,3,1,3;458410,1,1,3,3;287110,4,1,4,3;276739,2,4,3,3;207147,1,2,3,4;146882,1,1,3,2;137548,1,3,1,2;205973,4,1,1,2;205347,2,2,1,3;407213,4,1,1,3;111757,4,2,4,4;214753,2,1,1,1;423330,3,4,3,3;346175,4,1,4,4;305046,2,2,4,1;213102,1,3,1,1;443390,2,3,3,3;244222,3,3,3,1;245179,1,1,3,4;117506,2,3,2,4;190445,2,1,2,4;176303,3,1,2,1;240771,3,3,1,4;363851,3,4,4,2;206981,1,1,4,1;299605,2,1,3,3;457468,1,2,4,3;352242,1,4,2,1;444307,2,1,3,3;160376,2,2,4,2;485431,4,2,1,2;453972,3,3,4,3;202989,2,1,1,3;438282,3,4,2,4;490054,1,1,2,2;397395,1,1,2,3;240328,4,2,2,2;499270,1,3,4,1;465025,4,2,2,3;417590,1,2,4,4;188976,2,3,1,3;272392,4,4,2,2;423927,4,4,1,4;278516,3,3,1,1;120319,4,2,4,1;445179,2,1,3,3;138408,3,2,1,3;106318,4,1,4,3;138444,1,1,3,1;145097,3,1,1,2;495854,4,4,1,4;274012,1,4,3,3;167018,4,3,2,1;218219,3,2,3,4;329419,1,2,3,2;108514,1,1,4,1;186107,4,4,2,4;369057,1,1,3,1;461062,1,4,3,3;104673,1,1,3,2;463064,3,1,2,2;228802,2,1,2,2;383179,4,4,4,1;498575,4,2,3,2;445273,3,3,2,3;144250,2,2,3,4;298522,3,2,4,2;119306,2,1,4,4;402763,2,4,2,4;156269,4,4,3,2;117512,3,4,1,1;217492,4,1,1,1;228596,1,3,4,4;303953,1,4,2,2;160440,1,3,1,2;444045,3,2,2,4;362734,4,1,2,2;457199,3,2,1,1;390175,1,3,3,3;355108,3,3,4,1;260366,3,3,1,1;217192,2,3,1,3;357978,1,2,1,4;166541,3,3,1,4;109639,2,2,2,4;125169,3,2,3,1;237380,4,3,3,4;317118,3,1,2,3;280006,2,1,2,4;410177,2,3,3,4;398208,1,4,4,3;211730,4,3,2,4;105442,4,1,2,4;266893,2,3,2,4;316151,2,2,1,4;429162,3,4,2,4;414973,3,4,2,3;373728,1,2,2,1;401535,1,2,1,3;354030,2,2,1,4;461310,1,2,2,3;496987,1,3,1,1;481665,3,2,2,1;355186,4,3,4,4;337331,4,1,3,3;123777,2,3,3,1;492661,4,1,2,1;258113,2,2,4,1;319492,1,4,1,1;399666,3,1,4,4;476970,1,1,2,4;490605,4,4,4,2;475034,3,2,1,2;321855,3,2,4,3;498697,4,4,4,1;436437,1,2,3,3;104887,1,1,3,4;407201,2,4,1,2;136806,1,1,1,3;449179,1,2,1,3;447780,2,2,4,1;112827,4,4,1,1;323141,1,1,2,2;234307,1,4,2,3;246026,1,3,1,3;219714,2,4,2,1;368650,3,4,4,1;309322,1,1,2,2;495204,1,3,1,3;141214,4,1,4,3;101951,3,1,1,3;137169,1,4,2,1;250262,2,2,2,4;294619,2,2,4,4;250540,1,3,3,1;360720,4,3,2,3;187681,1,3,2,2;143402,3,3,1,2;252362,2,3,1,1;454494,2,2,1,3;427706,1,3,1,4;359923,4,1,3,4;388976,2,1,4,1;140156,2,4,4,4;459931,2,1,3,4;477808,2,2,2,4;248205,3,3,3,4;355132,1,4,3,1;103042,1,2,3,1;481606,1,2,4,2;345578,1,1,2,3;238947,4,4,1,2;473687,2,4,2,3;159439,3,3,3,1;429067,4,3,3,4;444958,3,4,4,4;474788,2,2,1,3;159366,2,2,2,1;397544,4,4,4,1;407677,1,2,2,3";
+
+    //convert string to string array (parse by semi-colon)
+    public static String[] electionDataByVoter= electionData.split(";");
+
+    /*
+     pres1= Mordy Golding
+     pres2= Shira Gotshalk
+     pres3= Stephanie Evans
+     pres4= Ray Villalobos
+
+     VP1= Dona Sarkar
+     VP2= Tyler Ahn
+     VP3= Joe Camp
+     VP4= Brandon LeBlanc
+
+     sec1= Jason Howard
+     sec2= Nikki Rubino
+     sec3= Peggy Fisher
+     sec4= Neil Edde
+
+     treas1= David Gassner
+     treas2= Sharon Bennett
+     treas3= Walt Ritscher
+     treas4= Heather Hurley
+    */
+
+    //create class-scoped variables for each candidate's vote tally, set each equal to 0
+    public static int pres1= 0, pres2= 0, pres3= 0, pres4= 0, VP1= 0, VP2= 0, VP3= 0, VP4= 0, sec1= 0, sec2= 0, sec3= 0, sec4= 0, treas1= 0, treas2= 0, treas3= 0, treas4= 0;
+
+    public static void main(String[] args) {
+
+        //create integer variable for number of valid votes and set it equal to 0
+        int realVotes= 0;
+
+        for (int i= 0; i< electionDataByVoter.length; i++) {
+
+            //voter ID is first six numbers of each set (charAt 0 to charAt 6), create substring for each
+            String voterID= electionDataByVoter[i].substring(0,6);
+
+            //defer to method repeatID method below to determine if there are any duplicate voter IDs (meaning someone voted more than once). Those votes will be discarded
+                if (repeatID(voterID)) {
+                    i++;
+                }
+
+                realVotes++;
+
+                //create string array with individual votes separated (parse by comma)
+                String[]indivVotes = electionDataByVoter[i].split(",");
+
+                //convert to integers (refer to tallyVotes method below)
+                tallyVotes(Integer.parseInt(indivVotes[1]), Integer.parseInt(indivVotes[2]), Integer.parseInt(indivVotes[3]), Integer.parseInt(indivVotes[4]));
+
+            }
+
+            System.out.println("Election results are as follows:");
+            System.out.println();
+
+            //Outputs for office of President
+            System.out.print("Office of President ");
+            int totalPresVotes= pres1+pres2+pres3+ pres4;
+            System.out.println("("+ totalPresVotes +" votes):");
+            System.out.println("Mordy Golding received " + pres1 + " votes.");
+            System.out.println("Shira Gotshalk received " + pres2 + " votes.");
+            System.out.println("Stephanie Evans received " + pres3 + " votes.");
+            System.out.println("Ray Villalobos received " + pres4 + " votes.");
+                //if pres1 vote tally was greatest
+            if (pres1 > pres2 && pres1 > pres3 && pres1 > pres4) {
+                System.out.println("New President- Mordy Golding\n");
+                //if pres2 vote tally was greatest
+            } else if (pres2 > pres1 && pres2 > pres3 && pres2 > pres4) {
+                System.out.println("New President- Shira Gotshalk\n");
+                //if pres3 vote tally was greatest
+            } else if (pres3 > pres1 && pres3 > pres2 && pres3 > pres4) {
+                System.out.println("New President- Stephanie Evans\n");
+                //if pres4 vote tally was greatest
+            } else if (pres4 > pres1 && pres4 > pres2 && pres4 > pres3) {
+                System.out.println("New President- Ray Villalobos\n");
+            }
+
+            //Outputs for office of Vice President
+            System.out.print("Office of Vice President ");
+            int totalVPVotes= VP1+VP2+VP3+VP4;
+            System.out.println("("+ totalVPVotes +" votes):");
+            System.out.println("Dona Sarkar received " + VP1 + " votes.");
+            System.out.println("Tyler Ahn received " + VP2 + " votes.");
+            System.out.println("Joe Camp received " + VP3 + " votes.");
+            System.out.println("Brandon LeBlanc received " + VP4 + " votes.");
+                //if VP1 vote tally was greatest
+            if (VP1 > VP2 && VP1 > VP3 && VP1 > VP4) {
+                System.out.println("New Vice President- Dona Sarkar\n");
+                //if VP2 vote tally was greatest
+            } else if (VP2 > VP1 && VP2 > VP3 && VP2 > VP4) {
+                System.out.println("New Vice President- Tyler Ahn\n");
+                //if VP3 vote tally was greatest
+            } else if (VP3 > VP1 && VP3 > VP2 && VP3 > VP4) {
+                System.out.println("New Vice President- Joe Camp\n");
+                //if VP4 vote tally was greatest
+            } else if (VP4 > VP1 && VP4 > VP2 && VP4 > VP3) {
+                System.out.println("New Vice President- Brandon LeBlanc\n");
+            }
+
+            //Outputs for office of Secretary
+            System.out.print("Office of Secretary ");
+            int totalSecVotes= sec1+sec2+sec3+sec4;
+            System.out.println("("+ totalSecVotes +" votes):");
+            System.out.println("Jason Howard received " + sec1 + " votes.");
+            System.out.println("Nikki Rubino received " + sec2 + " votes.");
+            System.out.println("Peggy Fisher received " + sec3 + " votes.");
+            System.out.println("Neil Edde received " + sec4 + " votes.");
+                //if sec1 vote tally was greatest
+            if (sec1 > sec2 && sec1 > sec3 && sec1 > sec4) {
+                System.out.println("New Secretary- Jason Howard\n");
+                //if sec2 vote tally was greatest
+            } else if (sec2 > sec1 && sec2 > sec3 && sec2 > sec4) {
+                System.out.println("New Secretary- Nikki Rubino\n");
+                //if sec3 vote tally was greatest
+            } else if (sec3 > sec1 && sec3 > sec2 && sec3 > sec4) {
+                System.out.println("New Secretary- Peggy Fisher\n");
+                //if sec4 vote tally was greatest
+            } else if (sec4 > sec1 && sec4 > sec2 && sec4 > sec3) {
+                System.out.println("New Secretary- Neil Edde\n");
+            }
+
+            //Outputs for office of Treasurer
+            System.out.print("Office of Treasurer ");
+            int totalTresVotes= treas1+treas2+treas3+ treas4;
+            System.out.println("("+ totalTresVotes +" votes):");
+            System.out.println("David Gassner received " + treas1 + " votes.");
+            System.out.println("Sharon Bennett received " + treas2 + " votes.");
+            System.out.println("Walt Ritscher received " + treas3 + " votes.");
+            System.out.println("Heather Hurley received " + treas4 + " votes.");
+                //if treas1 vote tally was greatest
+            if (treas1 > treas2 && treas1 > treas3 && treas1 > treas4) {
+                System.out.print("New Treasurer- David Gassner");
+                //if treas2 vote tally was greatest
+            } else if (treas2 > treas1 && treas2 > treas3 && treas2 > treas4) {
+                System.out.print("New Treasurer- Sharon Bennett");
+                //if treas3 vote tally was greatest
+            } else if (treas3 > treas1 && treas3 > treas2 && treas3 > treas4) {
+                System.out.print("New Treasurer- Walt Ritscher");
+                //if treas4 vote tally was greatest
+            } else if (treas4 > treas1 && treas4 > treas2 && treas4 > treas3) {
+                System.out.print("New Treasurer- Heather Hurley");
+            }
+        }
+
+        //method to test for voter fraud (check for repeated ID numbers)
+        public static boolean repeatID (String voterID){
+            int numberOfIDRepeats= 0;
+            for (int i = 0; i < electionDataByVoter.length; i++) {
+                if (voterID.equals(electionDataByVoter[i].substring(0, 6))) {
+                    numberOfIDRepeats++;
+                }
+            }
+            if (numberOfIDRepeats >= 2)
+                return true;
+                else
+                    return false;
+        }
+
+        //method to count votes by ID (1, 2, 3, 4)
+        public static void tallyVotes ( int presVote, int VPVote, int secVote, int treasVote){
+            if (presVote== 1){
+                pres1++;
+            } else if (presVote== 2){
+                pres2++;
+            } else if (presVote== 3){
+                pres3++;
+            } else if (presVote== 4){
+                pres4++;
+            }
+
+            if (VPVote== 1){
+                VP1++;
+            } else if (VPVote== 2){
+                VP2++;
+            } else if (VPVote== 3){
+                VP3++;
+            } else if (VPVote== 4){
+                VP4++;
+            }
+
+            if (secVote== 1){
+                sec1++;
+            } else if (secVote== 2){
+                sec2++;
+            } else if (secVote== 3){
+                sec3++;
+            } else if (secVote== 4){
+                sec4++;
+            }
+
+            if (treasVote== 1){
+                treas1++;
+            } else if (treasVote== 2){
+                treas2++;
+            } else if (treasVote== 3){
+                treas3++;
+            } else if (treasVote== 4){
+                treas4++;
+            }
+        }
+    }
